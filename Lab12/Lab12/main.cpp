c409d38da472d4b819d81328b39b527b9a0494de
Zaina <Zaina@DESKTOP-EI3LB00>
2020-04-26 17:55:21 -0400

    Qucik Sort - needs fixing

--- Lab12/Lab12/main.cpp
+++ Lab12/Lab12/main.cpp
@@ -19,35 +19,177 @@
 
 #include <iostream>
 #include <chrono>
+#include <cstdlib>
 
 using namespace std;
 
+// Swap function  
 template <class T>
-void bubbleSort(T* array, int size) {
+void swap(T* a, T* b)
+{
+	T t = *a;
+	*a = *b;
+	*b = t;
+}
+//Display Function
+template < class T>
+void display(T** myArray, const int size) {
+	for (int i = 0; i < 10; i++) {
+		cout << *myArray[i] << " ";
+	}
+}
+
+
+// Completed
+template<class T>
+T** bubbleSort(T** data, int size) {
+	bool hasSwapped = false;
+
+	for (int i = 1; i <= size; i++) {
+		for (int j = 0; j < size - i; j++) {
+
+			T* curr = data[j];
+			T* next = data[j + 1];
+
+			if(*curr > *next) {
+				swap(curr, next);
+				hasSwapped = true;
+			}
+		}
+		if(!hasSwapped) { //finished sorting
+			break;
+		}
+	}
+	return data;
+}
+
+template <class T>
+T** insertionSort(T** data, int size) {
+
+	for (int i = 1; i < size; i++) {
+		T* curr = data[i];
+		
+		for (int j = i - 1; j >= 0; j--) {
+			T* previous = data[j];
+
+			if (*curr < *previous) {
+				swap(curr, previous);
+				curr = data[j];
+			}
+			else {
+				break; //reached correct spot
+			}
+
+		}
+	}
+
+	return data;
+}
+
+template <class T>
+T** merge(T** firstHalf, int size1, T** secondHalf, int size2) {
+	T** result = new T * [size1 + size2];
+
+	for (int i = 0; i < size1; i++) {
+		result[i] = firstHalf[i];
+	}
+
+	for (int i = 0; i < size2; i++) {
+		result[i + size1] = secondHalf[i];
+	}
+
+	result = insertionSort(result, size1 + size1);
+
+	return result;
+}
+
+template <class T>
+T** mergeSort(T** data, int size) {
+
+	if (size <= 1) {
+		return data;
+	}
+
+	int size1 = size / 2;
+	int size2 = size - size1;
+
+	T** firstHalf = new T * [size1];
+	T** secondHalf = new T * [size2];
+	
+	for (int i = 0; i < size; i++) {
+		if (i < size1) {
+			firstHalf[i] = data[i];
+		}
+		else {
+			secondHalf[i - size1] = data[i];
+		}
+	}
+
+	T** firstSorted = mergeSort(firstHalf,size1);
+	T** secondSorted = mergeSort(secondHalf,size2);
+	
+	T** res = merge(firstSorted, size1, secondSorted, size2);
+	return res;
+}	
+
+
+/*******************************************************************
+* QUICK SORT - Always picks last element of the array as pivot    *
+* 
+*******************************************************************/
+
+
+template <class T>
+T** quickSort(T** data, int start, int size) {
+	if (start < size - 1) {
+		//Partitioning the array
+		T* pivot = data[size - 1];
+		display(data, size);
+		cout <<" Start " << start << " Size " << size << " Pivot " << *pivot << endl;
+		int i = start - 1;
+		for (int j = start; j < size - 1; j++) {
+			T* curr = data[j];
+			if (*curr < *pivot)
+			{
+				i++;
+				T* prev = data[i];
+				swap(prev, curr);
+			}
+		}
+		T* next = data[i + 1];
+		//Moving pivot to correct position
+		swap(next, pivot);
+		//Sorting partitions
+		quickSort(data, start, i);
+		quickSort(data, i+1, size);
+	}
+	return data;
+}
+
+template <class T>
+T** countingSort(T** array, int size) {
 
 }
 
 template <class T>
-void insertionSort(T* array, int size) {
-
-}
-
-template <class T>
-void mergeSort(T* array, int size) {
-
-}
-
-template <class T>
-void quickSort(T* array, int size) {
-
-}
-
-template <class T>
-void radixSort(T* array, int size) {
+T** radixSort(T** array, int size) {
 
 }
 
 
-int main(){
+int main() {
+	int** myArray = new int* [10];
+	for (int i = 0; i < 10; i++) {
+		myArray[i] = new int(rand() % 20);
+	}
+
+	for (int i = 0; i < 10; i++) {
+		cout << *myArray[i] << " ";
+	}
+	cout << endl;
+	quickSort(myArray, 0, 10);
+	
+	display(myArray, 10);
+
 	return 0;
 }
